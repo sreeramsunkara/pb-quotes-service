eafb058c27a6c9b28d3df61295286f25c7d3d0da
Sreeram Sunkara <sunkarasr@sunkarasr.local>
2020-05-14 15:28:45 -0400

    initial commit

---
+++ src/main/java/io/pivotal/quotes/QuotesApplication.java
@@ -0,0 +1,15 @@
+package io.pivotal.quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class QuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesApplication.class, args);
+    }
+}
+
